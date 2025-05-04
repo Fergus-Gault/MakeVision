b6db54eaef0eaa000fc057c54b892477da4f6f17
fergusgault <fergus@gault.com>
2025-05-04 16:48:17 +0100

    Improved example plugin

--- plugins/example_plugin/filter.py
+++ plugins/example_plugin/filter.py
@@ -1,7 +1,21 @@
 from makevision.core import Filter
+from collections import defaultdict
 
 class ExampleFilter(Filter):
 
     def apply(self, results):
+        """Apply the filter to the results."""
+        filtered_results = []
+        counts = defaultdict(int)
+
+        class_limits = {
+            "white": 1,
+            "black": 1,
+            "red": 7,
+            "yellow": 7,
+        }
+
+        for result in results:
+            pass
 
         return results
