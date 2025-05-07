f252e66173c81eb1b58647bfb5d4a300da753983
Fergus Gault <fergus@gault.com>
2025-05-07 19:14:32 +0100

    Added timer

---
+++ makevision/utils/timer.py
@@ -0,0 +1,38 @@
+import time
+from collections import defaultdict
+from typing import Optional
+
+
+class Timer:
+    _timings = defaultdict(list)
+
+    def __init__(self, name: Optional[str] = None, accumulate: bool = False):
+        self.name = name
+        self.accumulate = accumulate
+        self.start_time = 0
+
+    def __enter__(self):
+        self.start_time = time.perf_counter()
+        return self
+
+    def __exit__(self, *args):
+        self.stop()
+
+    def start(self):
+        self.start_time = time.perf_counter()
+
+    def stop(self):
+        elapsed = time.perf_counter() - self.start_time
+        if self.name:
+            if self.accumulate:
+                Timer._timings[self.name].append(elapsed)
+            # TODO: Log time
+        return elapsed
+
+    @classmethod
+    def summary(cls):
+        for name, records in cls._timings.items():
+            total_time = sum(records)
+            count = len(records)
+            avg = total_time / count if count > 0 else 0
+            # TODO: Log summary
