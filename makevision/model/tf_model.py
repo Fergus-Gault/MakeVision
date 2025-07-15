70d1a5e990ba454c48480f2596572e7e84c57444
Fergus Gault <fergus@gault.com>
2025-07-15 04:21:06 +0100

    Minor update

--- makevision/model/tf_model.py
+++ makevision/model/tf_model.py
@@ -4,8 +4,9 @@ from makevision.core import Model
 class TfModel(Model):
     def __init__(self, model_path: str):
         """Initialize the TensorFlow model with the given model path."""
-        pass
+        super().__init__(model_path)
 
     def load_model(self, model_path: str):
         """Load the TensorFlow model from the specified path."""
-        pass
+        import tensorflow as tf
+        self.model = tf.keras.models.load_model(model_path)
